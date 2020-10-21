4b03c28e8831b1d60cf12899b662659208d905f4
gzstyp <444141300@qq.com>
2020-10-21 22:12:01 +0800

    仅支持"GET","POST","OPTIONS"请求方式

--- service/src/main/java/com/fwtai/auth/WebMvcConfig.java
+++ service/src/main/java/com/fwtai/auth/WebMvcConfig.java
@@ -19,9 +19,9 @@ public class WebMvcConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(final CorsRegistry registry){
         registry.addMapping("/**")
-            .allowedOrigins("http://www.fwtai.com","https://www.fwtai.com","http://192.168.3.108","http://127.0.0.1:8080","http://192.168.3.108:8080","http://localhost:8080")//同源配置*表示任何请求都视为同源，若需指定ip和端口可以改为如“localhost：8080”，多个以“,”分隔；
-            .allowedMethods("PUT","DELETE","GET","POST","OPTIONS")
-            .allowedHeaders("*")
+            .allowedOrigins("http://www.fwtai.com","https://www.fwtai.com","http://192.168.3.108","http://127.0.0.1:8080","http://192.168.3.108:8080","http://api.i8id.com")//同源配置*表示任何请求都视为同源，若需指定ip和端口可以改为如“localhost：8080”，多个以“,”分隔；
+            .allowedMethods("GET","POST","OPTIONS")
+            .allowedHeaders("content-type")
             .allowCredentials(true)
             .maxAge(7200L);//默认值是 1800 seconds (即30分钟)
     }
