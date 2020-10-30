ed1b18b396b69f1c137019c7a0bc59a2dcd7cf51
gzstyp <444141300@qq.com>
2020-10-30 12:12:30 +0800

    通用的删除、批量删除

---
+++ service/src/main/java/com/fwtai/service/DataService.java
@@ -0,0 +1,42 @@
+package com.fwtai.service;
+
+import com.fwtai.bean.PageFormData;
+import com.fwtai.datasource.DaoHandle;
+import com.fwtai.tool.ToolClient;
+import com.fwtai.tool.ToolString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+/**
+ * 通用的删除、批量删除
+ * @作者 田应平
+ * @版本 v1.0
+ * @创建时间 2020-09-27 17:09
+ * @QQ号码 444141300
+ * @Email service@dwlai.com
+ * @官网 http://www.fwtai.com
+*/
+public class DataService{
+
+    @Autowired
+    private DaoHandle daoHandle;
+
+    protected final String delById(final PageFormData formData,final String sqlMapId){
+        final String p_kid = "id";
+        final String validateField = ToolClient.validateField(formData,p_kid);
+        if(validateField != null)return validateField;
+        return ToolClient.executeRows(daoHandle.execute(sqlMapId,formData.getString(p_kid)));
+    }
+
+    protected final String delByKeys(final PageFormData formData,final String sqlMapId){
+        final String p_ids = "ids";
+        final String validate = ToolClient.validateField(formData,p_ids);
+        if(validate != null)return validate;
+        final ArrayList<Object> lists = ToolString.jsonArrayToList(formData.get(p_ids));
+        if(lists.size() <= 0){
+            return ToolClient.createJsonFail("请选择要删除的数据");
+        }
+        return ToolClient.executeRows(daoHandle.execute(sqlMapId,lists),"操作成功","数据已不存在,刷新重试");
+    }
+}
