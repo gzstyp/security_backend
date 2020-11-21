c240ed9260e67a6b4e149e0321b871961eea401e
gzstyp <444141300@qq.com>
2020-11-21 23:37:08 +0800

    hasLength

--- service/src/main/java/com/fwtai/auth/LogoutService.java
+++ service/src/main/java/com/fwtai/auth/LogoutService.java
@@ -19,7 +19,7 @@ public class LogoutService implements LogoutHandler {
     @Override
     public void logout(final HttpServletRequest request,final HttpServletResponse response,final Authentication authentication) {
         final String token = request.getHeader(ConfigFile.ACCESS_TOKEN);
-        if (!StringUtils.isEmpty(token)) {
+        if (StringUtils.hasLength(token)) {
             System.out.println("accessToken = " + authentication);
             SecurityContextHolder.clearContext();
         }
